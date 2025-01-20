be052d790aab231b25562d40ed920cd4d3b494c1
Caio Barbosa <caiofelipebarbosa92@gmail.com>
2025-01-20 19:22:21 -0300

    Project created

---
+++ src/test/java/com/desafio_backend/game/GameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio_backend.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
